ee958531ed0d67383d471a6607e5a411c6e29a04
DIEGO163 <dah.fernandez@yavirac.edu.ec>
2022-08-06 00:54:20 -0500

    arreglar categoria

--- frontend/src/app/feature/feature.module.ts
+++ frontend/src/app/feature/feature.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { FeatureRoutingModule } from "./feature-routing.module";
 import { FeriadosFormComponent } from "./feriados/feriados-form/feriados-form.component";
+import { RolFormComponent } from './rol/rol-form/rol-form.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
+
 
 
 @NgModule({
     declarations: [
-        FeriadosFormComponent
+        FeriadosFormComponent,
+        RolFormComponent,
+        CategoriaFormComponent,
+
   ],
     imports: [
       CommonModule,
